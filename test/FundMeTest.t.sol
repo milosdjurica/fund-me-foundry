5a9b586dcdd0082e0ba05289ffddf3b3b076ac06
milosdjurica <milosdjurica13@gmail.com>
2023-11-26 22:10:06 +0100

    added first test for minimum dollar == 5e18

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -2,6 +2,18 @@
 
 pragma solidity ^0.8.19;
 
-import {Test} from "../lib/forge-std/src/Test.sol";
+import {Test, console} from "../lib/forge-std/src/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
 
-contract FundMeTest is Test {}
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    // this happends first
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+}
